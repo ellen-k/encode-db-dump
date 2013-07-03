b9d6573ad65b60c9040daccf85745e73dc9fbd4c
Ellen K <ellen@k.fake>
2013-07-03 17:50:25 -0400

    get-headings.rb produces smarter output
    Modified get-headings.rb to sort headings into unique per file
    versus multiple instances of heading per file,
    plus parse out 'special' headings eg GEO IDs.

--- sideways_dump/get-headings.rb
+++ sideways_dump/get-headings.rb
@@ -1,23 +1,53 @@
 #!/usr/bin/ruby
 
 # get the headings that we will make the giant graph out of
-# Note that this code handles files which have inappropriate embedded tabs and newlines
-# newly dumped files will no long have these problems.
 
-# for each submission
-# for each of the listed tables
-# grab the appropriate columns
+
+# INPUTS
+# Directory where submission tablespaces have been dumped to with dump_from_db.rb
+# basename for output lists
+# use -f to handle files with inappropriate newlines / tabs
+# which newly dumped file will not have.
+
+# OUTPUTS
+# basename.raw
+#   The raw headings, sorted by submission.
+# basename.uniq
+#   All headings which only appear once in every submission they appear in.
+# basename.multi
+#   All headings which appear at least twice in at least one submission
+# basename.special
+#   Special-cased headings -- see special_case_this_header
+#   Which require special handling. Mostly GEO ids.
 
 # Arguments
 if ARGV.length < 2 then
-  puts "Usage: ./get_headings.rb input_directory output_file [-f]
+  puts "Usage: ./get_headings.rb input_directory output_file_basename [-f]
         -f: Fix embedded newlines or tabs." 
   exit
 end
 
 basedir = ARGV[0]
-output_file = ARGV[1]
+baseout = ARGV[1]
 FIXTABS = ! ARGV[2].nil? # so technically it doesn't HAVE to be -f.
+
+
+# Does this header match 'special case' criteria
+# & thus should be excluded from the main tables
+def special_case_this_header?(header)
+
+  return true if( header =~ /^:Anonymous Datum #\d+$/)
+  return true if( header =~ /^GSM\d+:data_url$/ )# Just an URL to GEO, don't need
+  return true if( header =~ /^SRR\d+:data_url$/ )# Specific SRR ID
+  return true if( header =~ /^GSM\d+:GEO_record$/) # Geo ID
+  return true if( header =~ /^GEO:TMPID:.*$/) # Geo ID
+
+  # TODO more cases
+
+  return false
+end
+
+
 
 # table filename and the columns in it that we want
 tables = [
@@ -30,7 +60,12 @@ tables = [
 #subs = Dir.entries(basedir).reject{|s| s =~ /\./ }[0..50] # a small test
 subs = Dir.entries(basedir).reject{|s| s =~ /\./ }
 
-rawout = File.open(output_file, "w")
+fnames = Hash.new{|h, filetype| "#{baseout}.#{filetype}" } 
+
+
+rawout = File.open(fnames["raw"], "w")
+multiout = File.open(fnames["multi"], "w")
+singleout = File.open(fnames["uniq"], "w")
 
 subs.each{|s|
   tables.each{|t|
@@ -85,13 +120,40 @@ subs.each{|s|
       heads = fixed.map{|line|
         cols.split(",").map(&:to_i).map{|c| line[c]}.join(":")
       }
-      #rawout.puts "#{s}-#{tfile}-#{heads.join("\n")}"
+
+      # Check for uniqueness: Are there any nonunique key:heading pairs?
+      # If so, put them for the multiheading table.
+    
+      nonuniq,unique = heads.uniq.partition{|h| heads.count(h) > 1 }
+      multiout.puts nonuniq.join("\n")
+      singleout.puts unique.join("\n")
+
       rawout.puts heads.join("\n")
     end
   } 
 }
 
 rawout.close
+multiout.close
+singleout.close
 
+puts "Finished processing input...cleaning up lists."
 
+# Then, some file cleanup:
+# Remove all headers appearing in multiheaders from singleheaders
+
+multiheaders = File.open(fnames["multi"], "r").readlines.map{|s| s.chomp}.uniq
+singleheaders = File.open(fnames["uniq"], "r").readlines.map{|s| s.chomp}.uniq
+
+singleheaders.reject!{|s| multiheaders.include? s}
+
+# Then, separate out known special-case headers 
+
+specialheaders, singleheaders = singleheaders.partition{|s| special_case_this_header?(s) }
+
+# Write stuff to file
+
+File.open(fnames["multi"], "w").puts multiheaders.join("\n")
+File.open(fnames["uniq"], "w").puts singleheaders.join("\n")
+File.open(fnames["special"], "w").puts specialheaders.join("\n")
 
